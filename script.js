0e0f5212add246d746977d7d60f72597a7981202
kennedy <sferreira.kennedy@gmail.com>
2025-02-06 21:40:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+//currency cotation
+const cotation = {
+  USD: 5.5,
+  EUR: 6.5,
+  GPB: 7.5,
+};
+
+// DOM elements
 const form = document.querySelector("form");
 const amount = document.querySelector("#amount");
 const currency = document.querySelector("#currency");
+const footer = document.querySelector("footer");
 
 // manipulating the input value for recive just numbers
 amount.addEventListener("input", () => {
@@ -12,5 +21,18 @@ amount.addEventListener("input", () => {
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, cotation.USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, cotation.EUR, "€");
+      break;
+    case "BRL":
+      convertCurrency(amount.value, cotation.GPB, "£");
+      break;
+  }
 });
+
+// function to convert currency
+function convertCurrency(amount, price, symbol) {}
