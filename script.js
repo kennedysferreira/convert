4968ffc9849a9f332fc12ba89bff20bb7420ddf9
kennedy <sferreira.kennedy@gmail.com>
2025-02-06 21:32:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+const form = document.querySelector("form");
 const amount = document.querySelector("#amount");
+const currency = document.querySelector("#currency");
 
 // manipulating the input value for recive just numbers
 amount.addEventListener("input", () => {
   const charRegex = /\D+/g;
 
   amount.value = amount.value.replace(charRegex, "");
-})
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  
+});
